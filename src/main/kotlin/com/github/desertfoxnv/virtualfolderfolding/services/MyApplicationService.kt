f5d1c6e23834de2282853aeb4090d502de496440
GitHub Action <action@github.com>
2022-06-15 15:26:13 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/desertfoxnv/virtualfolderfolding/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.desertfoxnv.virtualfolderfolding.services
+
+import com.github.desertfoxnv.virtualfolderfolding.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/desertfoxnv/virtualfolderfolding/services/MyProjectService.kt
+++ src/main/kotlin/com/github/desertfoxnv/virtualfolderfolding/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.desertfoxnv.virtualfolderfolding.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.desertfoxnv.virtualfolderfolding.MyBundle
 
 class MyProjectService(project: Project) {
 
